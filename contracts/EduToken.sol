68d85cc3317fc1dddbe231ec337b1f7a1b72762f
joe-zhou322 <joe.zhou@flatfeecorp.com>
2025-03-14 14:55:03 -0700

    minor bug fixes

--- contracts/EduToken.sol
+++ contracts/EduToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract EduToken is ERC20, Ownable {
     constructor() ERC20("Education Token", "EDU") Ownable(msg.sender) {
         // Mint initial supply to the contract deployer
-        _mint(msg.sender, 10 * 10 ** decimals()); // amount to be changed later, mint fewer for cost-saving purposes
+        _mint(msg.sender, 10 * 10 ** decimals()); // Initial supply tokens
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
