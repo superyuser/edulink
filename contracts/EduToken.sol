e93fd70e8785bbd4a8eec316c8a2af22219dd37a
joe-zhou322 <joe.zhou@flatfeecorp.com>
2025-03-14 03:16:13 -0700

    Modified mint amount to save gas

--- contracts/EduToken.sol
+++ contracts/EduToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract EduToken is ERC20, Ownable {
     constructor() ERC20("Education Token", "EDU") Ownable(msg.sender) {
         // Mint initial supply to the contract deployer
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _mint(msg.sender, 10 * 10 ** decimals()); // amount to be changed later, mint fewer for cost-saving purposes
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
